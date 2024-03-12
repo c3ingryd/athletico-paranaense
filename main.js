7fad41b539d8d373244e08aa5354d7e33cae73cd
c3ingryd <162719105+c3ingryd@users.noreply.github.com>
2024-03-12 11:24:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://s2-ge.glbimg.com/-zcCqJkVirMvT_KGB_1M5MMqt74=/0x0:1300x867/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2019/U/N/AaifUGSOmBC4rtZBLikA/20190911-atletico-paranaense-x-internacional-22.jpg"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://www.bandab.com.br/wp-content/uploads/2024/02/Athletico-comemoracao-960x638.jpeg.webp"
+}
